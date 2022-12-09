f89038d235ac715b0cb88eec96a7f3f0dd1517f9
thiago <371426+dooart@users.noreply.github.com>
2022-12-09 19:43:20 -0300

    Upgrade all dependencies

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 export default function Home({}) {
   return (
     <Layout meta={{ title: "Index page" }}>
-      <div></div>
+      <div className="p-4 text-emerald-700">ok</div>
     </Layout>
   );
 }
